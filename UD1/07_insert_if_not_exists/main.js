111cc174a1961dbcd302ef08cc62c0ad2cac4a46
javierplo12 <javierplo841@gmail.com>
2024-10-02 19:59:08 +0200

    Ejercicio7

--- UD1/07_insert_if_not_exists/main.js
+++ UD1/07_insert_if_not_exists/main.js
@@ -1,18 +1,29 @@
-const assert = require('assert').strict
+const assert = require('assert').strict;
 
 function insertIfNotExists(array, item, position) {
-    return array
+    console.log(array.includes (item))
+ 
+    if (array.includes(item) == false) {
+      
+        if (position === true) {
+            array.unshift(item);
+        } else {
+        
+            array.push(item);
+        }
+    }
+    return array;
 }
 
 
-let array = ['pera', 'manzana']
+let array = ['pera', 'manzana'];
 
-let result = insertIfNotExists(array, 'pera', false)
-assert.deepStrictEqual(result, ['pera', 'manzana'])
+let result = insertIfNotExists(array, 'manzana', false);
+assert.deepStrictEqual(result, ['pera', 'manzana']);
 
-// result = insertIfNotExists(array, 'melón', false)
-// assert.deepStrictEqual(result, ['pera', 'manzana', 'melón'])
+result = insertIfNotExists(array, 'melón', false);
+assert.deepStrictEqual(result, ['pera', 'manzana', 'melón']);
 
-// result = insertIfNotExists(array, 'melocotón', true)
-// assert.deepStrictEqual(result, ['melocotón', 'pera', 'manzana', 'melón'])
+result = insertIfNotExists(array, 'melocotón', false);
+assert.deepStrictEqual(result, ['melocotón', 'pera', 'manzana', 'melón']);
 
