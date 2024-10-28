a2647eb5d10c8a7c9deda6e8f77ec435006a614d
javierplo12 <javierplo841@gmail.com>
2024-10-28 17:33:16 +0100

    .

---
+++ UD3/05_dom/main.js
@@ -0,0 +1,8 @@
+window.onload= () => {
+    const allDivs = document.querySelectorAll('div')
+    for (let div of allDivs){
+        console.log(div)
+
+    }
+    const allBButons = document.querySelectorAll('.buttons')
+}
--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,28 @@
-window.onload = function(e) {
+window.onload = function (e) {
     console.log('documento cargado')
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    document.getElementsByTagName('h2')[1].innerText = 'titulo cambiado'
+    document.getElementById('username');
+    const firstElements = document.querySelectorAll("article .first");
+
+    firstElements.forEach(element => {
+        element.style.color = "red"; 
+    })
+
+    document.querySelectorAll('li.item')
+
+
+    const buttonsInButtonsClass = document.querySelectorAll(".buttons button");
+    
+    buttonsInButtonsClass.forEach(button => {
+        button.innerText = "Nuevo Texto"; // Cambia el texto del botón
+        button.style.backgroundColor = "blue"; // Cambia el color de fondo del botón
+    });
+    
+    
+
+
+
+
+
 }
